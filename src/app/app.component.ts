7aa4c65a2478c47148e91595dcffad5a02c1e657
Isaac Hamm <isaachamm3@gmail.com>
2025-02-13 21:27:08 -0700

    0.0.1 -- got an initial version of film details input organized, but not stylized or functional.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilmDetailsInputComponent } from './components/journal-details-input/film-details-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FilmDetailsInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/journal-details-input/film-details-input.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'film-details-input',
+  templateUrl: './film-details-input.component.html',
+  styleUrl: './film-details-input.component.scss',
+})
+export class FilmDetailsInputComponent {}
